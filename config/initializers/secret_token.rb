88db183fc75e5a42357f41cc36d33657dc30b3ff
Ward W. Vuillemot <wwv@mac.com>
2013-12-15 11:22:06 -0800

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+UnigonRails::Application.config.secret_token = 'cf5c0b91d035c4c13fc3c1c95ce611dc6fd20b7e35d1e89e3941a76b217baa3c3da5c831efc32562b3aa17e2f71a9513f50d436b21c24b8db5d1829206bb9a86'
